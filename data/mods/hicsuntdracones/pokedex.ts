a6965b7df7b71123e296c8bd1ef36026e092506c
AayLith <bzh-artikodin@hotmail.fr>
2026-07-08 15:48:11 +0200

    t

--- data/mods/hicsuntdracones/pokedex.ts
+++ data/mods/hicsuntdracones/pokedex.ts
@@ -18,7 +18,7 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
             "0": "Aftermath",
             "1": "Perish Body"
         },
-        "height": 1,
+        "height": 1.7,
         "weight": 40
     },
     "swedusa": {
@@ -39,7 +39,9 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
         "abilities": {
             "0": "Toxic Chain",
             "1": "Corrosion"
-        }
+        },
+        "height": 1.4,
+        "weight": 50
     },
     "excalibirb": {
         "num": 3,
@@ -59,7 +61,9 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
         "abilities": {
             "0": "Dauntless Shield",
             "1": "Intrepid Sword"
-        }
+        },
+        "height": 2.2,
+        "weight": 190
     },
     "pentragon": {
         "num": 4,
@@ -81,7 +85,9 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
         "abilities": {
             "0": "Neuroforce",
             "1": "Tinted Lens"
-        }
+        },
+        "height": 1.8,
+        "weight": 60
     },
     "artikodin": {
         "num": 144,
@@ -104,8 +110,8 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
             "0": "Snow Warning",
             "1": "Ice Scales"
         },
-        "height": 0,
-        "weight": 0
+        "height": 1.7,
+        "weight": 55.4
     },
     "sulfura": {
         "num": 146,
@@ -128,8 +134,8 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
             "0": "Drought",
             "1": "Flash Fire"
         },
-        "height": 0,
-        "weight": 0
+        "height": 2.2,
+        "weight": 60
     },
     "electhor": {
         "num": 145,
@@ -152,7 +158,167 @@ export const Pokedex: {[speciesid: string]: ModdedSpeciesData} = {
             "0": "Drizzle",
             "1": "Lightning Rod"
         },
-        "height": 0,
-        "weight": 0
+        "height": 1.6,
+        "weight": 52.6
+    },
+    "grahyna": {
+        "num": 5,
+        "name": "Grahyèna",
+        "types": [
+            "Dark",
+            "Ice"
+        ],
+        "heightm": 1,
+        "weightkg": 10,
+        "baseStats": {
+            "hp": 70,
+            "atk": 125,
+            "def": 70,
+            "spa": 90,
+            "spd": 60,
+            "spe": 115
+        },
+        "abilities": {
+            "0": "Slush Rush",
+            "1": "Intimidate",
+            "H": "Sharpness"
+        },
+        "height": 1.1,
+        "weight": 45
+    },
+    "sirenade": {
+        "num": 6,
+        "name": "Sirenade",
+        "types": [
+            "Water",
+            "Flying"
+        ],
+        "heightm": 1,
+        "weightkg": 10,
+        "baseStats": {
+            "hp": 108,
+            "atk": 85,
+            "def": 91,
+            "spa": 112,
+            "spd": 84,
+            "spe": 66
+        },
+        "abilities": {
+            "0": "Punk Rock"
+        }
+    },
+    "hummingbolt": {
+        "num": 7,
+        "name": "Hummingbolt",
+        "types": [
+            "Electric",
+            "Fairy"
+        ],
+        "heightm": 1,
+        "weightkg": 10,
+        "baseStats": {
+            "hp": 40,
+            "atk": 70,
+            "def": 60,
+            "spa": 140,
+            "spd": 80,
+            "spe": 150
+        },
+        "abilities": {
+            "0": "Levitate",
+            "1": "Sap Sipper"
+        },
+        "height": 0.2,
+        "weight": 1
+    },
+    "demanta": {
+        "num": 8,
+        "name": "Demanta",
+        "types": [
+            "Flying",
+            "Ground"
+        ],
+        "heightm": 1,
+        "weightkg": 10,
+        "baseStats": {
+            "hp": 85,
+            "atk": 60,
+            "def": 80,
+            "spa": 90,
+            "spd": 140,
+            "spe": 75
+        },
+        "abilities": {
+            "0": "Sand Stream",
+            "1": "Serene Grace"
+        },
+        "height": 2.1,
+        "weight": 220
+    },
+    "naheulpoule": {
+        "num": 9,
+        "name": "Naheulpoule",
+        "types": [
+            "Normal"
+        ],
+        "heightm": 1,
+        "weightkg": 10,
+        "baseStats": {
+            "hp": 135,
+            "atk": 251,
+            "def": 50,
+            "spa": 50,
+            "spd": 50,
+            "spe": 74
+        },
+        "abilities": {
+            "0": "Anger Point"
+        },
+        "height": 0.5,
+        "weight": 5
+    },
+    "embvern": {
+        "num": 10,
+        "name": "Embvern",
+        "types": [
+            "Fire"
+        ],
+        "heightm": 1,
+        "weightkg": 10,
+        "baseStats": {
+            "hp": 1,
+            "atk": 100,
+            "def": 50,
+            "spa": 110,
+            "spd": 50,
+            "spe": 110
+        },
+        "abilities": {
+            "0": "Sturdy"
+        }
+    },
+    "adilon": {
+        "num": 11,
+        "name": "Adilon",
+        "types": [
+            "Grass",
+            "Water"
+        ],
+        "heightm": 1,
+        "weightkg": 10,
+        "baseStats": {
+            "hp": 140,
+            "atk": 90,
+            "def": 60,
+            "spa": 120,
+            "spd": 70,
+            "spe": 70
+        },
+        "abilities": {
+            "0": "Swift Swim",
+            "1": "Natural Cure"
+        },
+        "height": 16,
+        "weight": 500
     }
 };

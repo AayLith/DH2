a19b6a8df64db4a6bbb3e9ec6f907bc56890bb51
AayLith <bzh-artikodin@hotmail.fr>
2026-07-08 19:49:47 +0200

    format

--- data/mods/hicsuntdracones/formats-data.ts
+++ data/mods/hicsuntdracones/formats-data.ts
@@ -1,1 +1,58 @@
-export const Learnsets: {[speciesid: string]: ModdedLearnsetData} = export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {    "krakalong": {        "tier": "HSD",        "doublesTier": "HSD"    },    "swedusa": {        "tier": "HSD",        "doublesTier": "HSD"    },    "excalibirb": {        "tier": "HSD",        "doublesTier": "HSD"    },    "pentragon": {        "tier": "HSD",        "doublesTier": "HSD"    },    "artikodin": {        "tier": "HSD",        "doublesTier": "HSD"    },    "sulfura": {        "tier": "HSD",        "doublesTier": "HSD"    },    "electhor": {        "tier": "HSD",        "doublesTier": "HSD"    },    "grahyna": {        "tier": "HSD",        "doublesTier": "HSD"    },    "sirenade": {        "tier": "HSD",        "doublesTier": "HSD"    },    "hummingbolt": {        "tier": "HSD",        "doublesTier": "HSD"    },    "demanta": {        "tier": "HSD",        "doublesTier": "HSD"    },    "naheulpoule": {        "tier": "HSD",        "doublesTier": "HSD"    },    "embvern": {        "tier": "HSD",        "doublesTier": "HSD"    },    "adilon": {        "tier": "HSD",        "doublesTier": "HSD"    }};;
+export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
+	krakalong: {
+		tier: "HSD",
+		doublesTier: "HSD",
+	},
+	swedusa: {
+		tier: "HSD",
+		doublesTier: "HSD",
+	},
+	excalibirb: {
+		tier: "HSD",
+		doublesTier: "HSD",
+	},
+	pentragon: {
+		tier: "HSD",
+		doublesTier: "HSD",
+	},
+	artikodin: {
+		tier: "HSD",
+		doublesTier: "HSD",
+	},
+	sulfura: {
+		tier: "HSD",
+		doublesTier: "HSD",
+	},
+	electhor: {
+		tier: "HSD",
+		doublesTier: "HSD",
+	},
+	grahyna: {
+		tier: "HSD",
+		doublesTier: "HSD",
+	},
+	sirenade: {
+		tier: "HSD",
+		doublesTier: "HSD",
+	},
+	hummingbolt: {
+		tier: "HSD",
+		doublesTier: "HSD",
+	},
+	demanta: {
+		tier: "HSD",
+		doublesTier: "HSD",
+	},
+	naheulpoule: {
+		tier: "HSD",
+		doublesTier: "HSD",
+	},
+	embvern: {
+		tier: "HSD",
+		doublesTier: "HSD",
+	},
+	adilon: {
+		tier: "HSD",
+		doublesTier: "HSD",
+	},
+};
